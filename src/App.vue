9712535f45a8eaae26615f757e272ef0f185a7c7
Yaz Khoury <yaz.khoury@gmail.com>
2019-05-22 11:31:45 -0400

    feat: Add initial vue site

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Ethercluster: Open Source Ethereum-based Cloud Architecture"/>
   </div>
 </template>
 
@@ -24,5 +24,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  width: 200px;
+  height: 300px;
 }
 </style>
