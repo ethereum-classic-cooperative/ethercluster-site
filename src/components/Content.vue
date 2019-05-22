e33f04d00f1a1f2845b96c74c8b073e5284d618d
Yaz Khoury <yaz.khoury@gmail.com>
2019-05-22 16:23:59 -0400

    feat: Add Content template

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,3 +1,38 @@
 <template>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Cutting-edge Cloud Infrastructure Tools</h3>
+                Ethercluster is built using state-of-the-art cloud infrastructure tools. This allows tracking every changes in your public RPC through code for easy version-control.
+                <hr>
+                <br>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <img src="../assets/docker.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="../assets/kubernetes.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="../assets/terraform.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <h3>Node Scalability Built-In</h3>
+                Use the power of Kubernetes to instantly increase clusters when you need to expand your ethereum-based node bandwidth. Scale down when you don't need it. The choice is yours.
+                <hr>
+                <br>
+                <img src="../assets/scale.png" class="img-fluid" alt="">
+            </div>
+            <div class="col-sm-4">
+                <h3>Open-Source and Decentralized</h3>
+                By running your own ethercluster for your own organization's needs, you ensure you have Decentralized nodes because you're not connecting to an endpoint everyone is using. 
+                Because it's open source, you're always free to use it and customize it anyway you want.
+                <hr>
+                <br>
+                <img src="../assets/open-source.png" class="img-fluid" alt="">
+            </div>
+        </div>
+    </div>
 </template>
